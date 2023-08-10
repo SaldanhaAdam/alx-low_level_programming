8b718c4e3adb37b0ffbb295d32d3eb04cb34a2b0
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-10 11:55:52 -0700

    My first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,");
+    printf(" followed by a new line.\n");
+    
+    return 0;
+}
