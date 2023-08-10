3c9e622caf760d1c0fe5e6d9c5f1fdb0747b4ca7
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-10 11:59:44 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main() {
     printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,");
     printf(" followed by a new line.\n");
