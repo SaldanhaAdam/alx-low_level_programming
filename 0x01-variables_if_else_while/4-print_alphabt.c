69098f0a6c1a6101eb06a2e70078ba748e3483d4
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-14 15:20:06 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: lower case alphabets ecxept q and e
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != ''q' && letter != 'e')
+{
+	putchar(letter);
+}
+	letter++;
+}
+	putchar('\n');
+
+	return (0);
+}
