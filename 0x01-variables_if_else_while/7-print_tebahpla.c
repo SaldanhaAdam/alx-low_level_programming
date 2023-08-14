fca98fbc11feedda3fd5e78e90713a161a88db51
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-14 15:49:40 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
