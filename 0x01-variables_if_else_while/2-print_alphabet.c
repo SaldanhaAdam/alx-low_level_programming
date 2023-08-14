4957ad4b388d9c81d6b4c70b9352acfb57310ca3
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-14 15:04:49 -0700

    c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
 	 char letter = 'a';
 
@@ -18,7 +18,7 @@ int main()
 	letter++;
 }
 
-	 putchar('\n');
+	putchar('\n');
 
-	 return (0);
+	return (0);
 }
