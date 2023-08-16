9cf2604f01ef75bd87f27d0e0e71f21ce696fbcf
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-16 05:41:07 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet - A function that prints the alphabet
+ *
+*/
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
