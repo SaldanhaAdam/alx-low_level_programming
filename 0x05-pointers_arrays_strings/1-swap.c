a4ff202ac7060e9771bb19fbb03dad73aac4dea4
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-21 06:30:55 -0700

    c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * Description: A function that swaps the values of two integers
+ *
+ * @a: input parameter
+ * @b: input parameter
+ *
+ * Return: Always 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	int letter;
+
+	letter = *a;
+	*a = *b;
+	*b = letter
+	return (0);
+}
