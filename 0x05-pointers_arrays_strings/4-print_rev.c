b31375cd6f950222d9e6aad08ca1c87f136f4b6e
SaldanhaAdam <saldanha.adamsifeddine@gmail.com>
2023-08-21 06:54:32 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ *
+ * @s: string print parameter
+ *
+ * Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int j = 0;
+
+	while (s[j])
+		j++;
+
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
